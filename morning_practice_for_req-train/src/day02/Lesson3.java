32a0aefcac8329632820e7b5672a2e6e114c0fd3
user <user@WIN-0J1N56A7B78>
2025-11-14 10:23:40 +0900

    プッシュ

--- morning_practice_for_req-train/src/day02/Lesson3.java
+++ morning_practice_for_req-train/src/day02/Lesson3.java
@@ -15,17 +15,49 @@
 
 package day02;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Lesson3 {
 
 	public static void main(String[] args) throws IOException {
 
-		int sum = 0;// 合計金額
-		int average = 0;// 平均金額
-		int inputCount = 0;//入力した回数
+		//int sum = 0;// 合計金額
+		//int average = 0;// 平均金額
+		//int inputCount = 0;//入力した回数
 
 		/*ここから記入*/
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("金額①を入力してください");
+		String str = reader.readLine();
+		
+		System.out.println("金額②を入力してください");
+		String str1 = reader.readLine();
+		
+		System.out.println("金額③を入力してください");
+		String str2 = reader.readLine();
+		
+		int num = Integer.parseInt(str);
+		int num1 = Integer.parseInt(str1);
+		int num2 = Integer.parseInt(str2);
+		
+		//消費税の定義
+		float f =1.08f;
+		
+		//合計金額の算出
+		int sum = (int)((num+num1+num2)*f);
+		
+		
+		System.out.println("①から③の合計金額は税込み" + sum + "円");
+		
+		//平均金額の算出
+		int average = (int)(((num*f)+(num1*f)+(num2*f)) / 3 );
+		System.out.println("①から③の平均は税込み" + average + "円");
+		
 
 	}
 
